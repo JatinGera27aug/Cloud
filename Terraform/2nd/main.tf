f458f9bb38b262fd843bf4a2a8d26ef5f12806fc
JatinGera27aug <jatingera2004@gmail.com>
2025-01-19 13:45:39 +0530

    variables in terraform

---
+++ Terraform/2nd/main.tf
@@ -0,0 +1,48 @@
+provider "aws" { 
+    region  = "us-west-1"
+    access_key = "AKIAU6GD326R4WSUJE22"
+    secret_key = "dEpfBKmCAvSyks45WL29dcQ4idws54Jr6Fn/IUmH"
+}
+
+# resource "aws_instance" "ec2_example" { 
+#     ami = "ami-07d2649d67dbe8900" // image of os
+#     instance_type = var.instance_type   // CPU
+#     tags   ={
+#         Name = "terraform-ec2"
+#     }    
+# }
+
+resource "aws_instance" "ec2_example" { 
+    ami = "ami-07d2649d67dbe8900" // image of os
+    instance_type = var.instance_type   // CPU
+   tags = var.project_env
+}
+
+resource "aws_iam_user" "user" {
+  count = length(var.users)
+  name = var.users[count.index]
+}
+
+variable "users" {
+    description = "List of users"
+    type = list(string)
+    default = ["user1", "user2"]
+}
+
+variable "project_env" {
+    description = "Project environment"
+    type = map(string)   #dictionary type , key = project, value = project-alpha 
+    default = {
+        project = "project-alpha",
+        environment = "dev"
+    }
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type = string
+  default = "t2.micro"
+
+}
+
+
